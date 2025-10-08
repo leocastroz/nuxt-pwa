65efb32e39994a50a3823e6183eb7bf4a7e68e33
leocastroz <castroz.dev@gmail.com>
2025-10-08 02:43:32 -0300

    fix: testando.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,57 +16,51 @@ export default defineNuxtConfig({
     }
   },
   pwa: {
+    registerType: 'autoUpdate',
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
     manifest: {
       name: 'Nuxt PWA',
       short_name: 'NuxtPWA',
       description: 'My awesome Nuxt PWA!',
       theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait',
+      scope: '/',
+      start_url: '/',
       lang: 'en',
       icons: [
         {
-          src: "icons/icon64.png",
+          src: "/icons/icon64.png",
           sizes: "64x64",
           type: "image/png",
         },
         {
-          src: "icons/icon144.png",
+          src: "/icons/icon144.png",
           sizes: "144x144",
           type: "image/png",
         },
         {
-          src: "icons/icon192.png",
+          src: "/icons/icon192.png",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "icons/icon512.png",
+          src: "/icons/icon512.png",
           sizes: "512x512",
           type: "image/png",
         },
       ]
     },
-    workbox: {
-      navigateFallback: '/',
-      // cleanupOutdatedCaches: true,
-      // runtimeCaching: [
-      //   {
-      //     urlPattern: 'https://my-cdn.com/.*',
-      //     handler: 'CacheFirst',
-      //     options: {
-      //       cacheName: 'my-cache',
-      //       expiration: {
-      //         maxEntries: 10,
-      //         maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
-      //       },
-      //     },
-      //   },
-      // ],
+    devOptions: {
+      enabled: true,
+      type: 'module',
     },
   },
-  // devOptions: {
-  //   enabled: true,
-  //   type: 'module',
-  // },
+
   image: {
     // Options
   },
